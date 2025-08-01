e0007669cf2a42c620ae6e4bb1068b04781df228
mash-404 <mashsun404@gmail.com>
2025-08-02 08:13:17 +0900

    add

--- routes/dog.js
+++ routes/dog.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const request = require('request');
 
 router.get('/', async (req, res) => {
-request('https://dog.ceo/api/breeds/image/random ', function (error, response, body) {
+request('https://dog.ceo/api/breeds/image/random', function (error, response, body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
 res.json(data);
